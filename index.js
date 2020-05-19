f41eb536a5cbec66e4265309fc7b424720316a53
Arno736 <52139210+Arno736@users.noreply.github.com>
2020-05-19 14:39:01 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-module.exports = function no_guardian(mod)
+module.exports = function simple_guardian(mod)
 {
 	let active = true;
 	let command = mod.command;
@@ -16,12 +16,18 @@ module.exports = function no_guardian(mod)
 			if (event.cleared != "0")
 				command.message("Chest : " + event.cleared + " /  40");
 
-			if (event.cleared == "40")
-				command.message("Guardians Chest Full !");
+			if (event.cleared == "40") {
+				mod.toClient('S_CHAT', 3, {
+					channel: 21,
+					gm: 1,
+					name: 'SimpleGuardian',
+					message: String("Guardians Chest Full !")
+					});
+			}
 		}
 	});
 
-	mod.command.add('sg', (y) =>{
+	mod.command.add(['sg', 'simpleguardian'], (y) =>{
 		if (!y) {
 			active = !active;
 			command.message("Disable Guardians Optimisation : " + active.toString());
